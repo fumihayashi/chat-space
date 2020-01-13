a5f38b66c86aad3951d7f4e12bb549a03faccf11
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-01-14 05:02:43 +0900

    create HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,41 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message__upper-info">
+            <div class="message__upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="message__upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="message__upper-info__text">
+            ${message.content}
+          </p>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message__upper-info">
+            <div class="message__upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="message__upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="message__upper-info__text">
+            ${message.content}
+          </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -12,6 +49,11 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);      
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
   });
-
 });
